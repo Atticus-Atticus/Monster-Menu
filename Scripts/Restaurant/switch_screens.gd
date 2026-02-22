c222c59beb92dd92e6b449499f3cbf7f5398bdd2
JackSmart07 <up2523534@myport.ac.uk>
2026-02-22 18:34:27 +0000

    22/2/26
    - started camera switch code. doesnt work yet heh

--- Scripts/Restaurant/switch_screens.gd
+++ Scripts/Restaurant/switch_screens.gd
@@ -1,12 +1,23 @@
 extends Control
 
+@export var cameras : Array[Camera2D]=[]
+var current_index = 0
 
-
-
-
+#----------------------------------FUNCTIONS----------------------------------
 func _on_left_button_pressed() -> void:
 	print("going left")
-
+	current_index += 1
+	updateCamera()
 
 func _on_right_button_pressed() -> void:
 	print("going right")
+	current_index -= 1
+	updateCamera()
+
+
+#attempting camera switch. heh. https://www.youtube.com/watch?v=jlZmKwmguFM
+func updateCamera():
+	for i in range(cameras.size()):
+		if (i == current_index):
+			cameras[i].is_current()
+		#cameras[i].current = (i == current_index) % cameras.size()
