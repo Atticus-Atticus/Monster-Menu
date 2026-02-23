ca872b7eb1b733b1bd9e20e6a444c529831caf06
JackSmart07 <up2523534@myport.ac.uk>
2026-02-23 17:58:38 +0000

    23/2/26
    - cameras DONE. for now...
    - set up cameras for front of house/kitchen/backdoor

--- Scripts/Restaurant/switch_screens.gd
+++ Scripts/Restaurant/switch_screens.gd
@@ -2,22 +2,35 @@ extends Control
 
 @export var cameras : Array[Camera2D]=[]
 var current_index = 0
+var temp_index = 0
 
-#----------------------------------FUNCTIONS----------------------------------
-func _on_left_button_pressed() -> void:
-	print("going left")
-	current_index += 1
+func _ready():
+	#initialising, just in case
 	updateCamera()
 
-func _on_right_button_pressed() -> void:
-	print("going right")
+#----------------------------------FUNCTIONS----------------------------------
+func _on_left_button_pressed():
+	print("going left")
 	current_index -= 1
+	print("current index is ", current_index)
+	updateCamera()
+
+func _on_right_button_pressed():
+	print("going right")
+	current_index += 1
+	print("current index is ", current_index)
 	updateCamera()
 
 
 #attempting camera switch. heh. https://www.youtube.com/watch?v=jlZmKwmguFM
 func updateCamera():
+	if (current_index > (cameras.size()-1)):
+		current_index = 0
+	if (current_index < 0):
+		current_index = cameras.size()-1
+		
+	temp_index = sqrt(current_index*current_index)
+	print(temp_index)
 	for i in range(cameras.size()):
-		if (i == current_index):
-			cameras[i].is_current()
-		#cameras[i].current = (i == current_index) % cameras.size()
+		if (i == temp_index):
+			cameras[i].make_current()
