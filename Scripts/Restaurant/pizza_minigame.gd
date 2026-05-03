d22c212de0196a2cedc6bec3f038296e65ca2151
JackSmart07 <up2523534@myport.ac.uk>
2026-05-03 16:46:42 +0100

    3/5/26
    - implemented more assets
    - custom cursor 4 minigame
    - amended fire animation

--- Scripts/Restaurant/pizza_minigame.gd
+++ Scripts/Restaurant/pizza_minigame.gd
@@ -1,16 +1,22 @@
 extends Node2D
 
 @export var roundsLeft: int = 10
-@onready var leftTimer = $Hobs/Area2D_Left/Collision_Left/Left_Timer
-@onready var rightTimer = $Hobs/Area2D_Right/Collision_Right/Right_Timer
+@onready var leftTimer = $Area2D_Left/Collision_Left/Left_Timer
+@onready var rightTimer = $Area2D_Right/Collision_Right/Right_Timer
 @onready var randomTimer = $Random_Timer
 var RanNumGen = RandomNumberGenerator.new()
 var score = 1000
 var leftInteractable = false
 var rightInteractable = false
+@onready var cursorOpen = preload("res://Assets/Restaurant Assets/bellow open small.png")
+@onready var cursorClosed = preload("res://Assets/Restaurant Assets/bellow closed small.png")
 
 
 func _ready() -> void:
+	# custom mouse cursor when the minigame is open!
+	Input.set_custom_mouse_cursor(cursorOpen, 0, Vector2(256, 240))
+	
+	# starting fire randomiser
 	RanNumGen.randomize()
 	_random_timeout()
 	randomTimer.timeout.connect(_random_timeout)
@@ -19,6 +25,16 @@ func _ready() -> void:
 	leftTimer.timeout.connect(left_timeout)
 	rightTimer.timeout.connect(right_timeout)
 
+func _input(event):
+	# the bellow gets closed when the player clicks
+	if event is InputEventMouseButton:
+		if event.pressed:
+			# change to the closed bellow
+			Input.set_custom_mouse_cursor(cursorClosed)
+		else:
+			# change back to the open bellow
+			Input.set_custom_mouse_cursor(cursorOpen)
+
 #----------------------------------FUNCTIONS-----------------------------
 func endMinigame():
 	if roundsLeft <= 0:
@@ -26,6 +42,7 @@ func endMinigame():
 		RestaurantGlobals.orderFulfilled = true
 		Globals.resScore += score
 		Globals.tempSlimeballNum -= 2
+		Input.set_custom_mouse_cursor(null)
 		queue_free()
 
 func _random_timeout():
@@ -48,30 +65,30 @@ func _random_timeout():
 
 func left_timeout():
 	score -= 100
-	$Audio/Audio_FireOut.play()
 	$AnimationPlayer_Left.play("RESET")
+	$Audio/Audio_FireOut.play()
 	leftInteractable = false
 
 func right_timeout():
 	score -= 100
-	$Audio/Audio_FireOut.play()
 	$AnimationPlayer_Right.play("RESET")
+	$Audio/Audio_FireOut.play()
 	rightInteractable = false
 
 func _on_area_2d_left_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed() and leftInteractable == true:
 		score = score - int(((leftTimer.wait_time - leftTimer.time_left) / leftTimer.wait_time) * 100)
 		print(score)
+		$AnimationPlayer_Left.play("RESET")
 		leftTimer.stop()
 		$Audio/Audio_BlowWind.play()
-		$AnimationPlayer_Left.play("RESET")
 		leftInteractable = false
 
 func _on_area_2d_right_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed() and rightInteractable == true:
 		score = score - int(((rightTimer.wait_time - rightTimer.time_left) / rightTimer.wait_time) * 100)
 		print(score)
+		$AnimationPlayer_Right.play("RESET")
 		rightTimer.stop()
 		$Audio/Audio_BlowWind.play()
-		$AnimationPlayer_Right.play("RESET")
 		rightInteractable = false
