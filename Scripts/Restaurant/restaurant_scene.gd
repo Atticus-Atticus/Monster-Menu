fb0cfe4a6e534c6d0a0782f47e74a82beab18c5d
JackSmart07 <up2523534@myport.ac.uk>
2026-05-10 01:17:24 +0100

    10/5/26
    - added tutorial dialogue!!
    - ACTUALLY FINISHED!!

--- Scripts/Restaurant/restaurant_scene.gd
+++ Scripts/Restaurant/restaurant_scene.gd
@@ -3,10 +3,11 @@ extends Node2D
 #gets an ref 2 the spawner so i can call it here.
 @onready var Spawner = $NPC_Spawner
 @onready var NPC = preload("res://Scenes/Restaurant specific/npc.tscn")
-
+var dialogueResource = preload("res://Scripts/Restaurant/Dialogues/NPCDialogue_1.dialogue")
 var canCustomerOrder = true
 var orderFulfilled = false
 
 func _ready():
 	Globals.customersLeft = (Playerdata.slimeballs_collected / 2)
 	$"../CanvasLayer/Switch_Screens".updateCustomers()
+	DialogueManager.show_example_dialogue_balloon(dialogueResource, "tutorialDialogue")
